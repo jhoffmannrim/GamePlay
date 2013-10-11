54dc5be123c3889f3c180112db15219bb5c28054
Jonathan Hoffmann <jhoffmann@blackberry.com>
2013-10-11 11:31:32 -0400

    add meshtest

---
+++ tests/meshtest/src/MeshTest.h
@@ -0,0 +1,62 @@
+#ifndef MESHTEST_H_
+#define MESHTEST_H_
+
+#include "gameplay.h"
+
+using namespace gameplay;
+
+/**
+ * Main game class.
+ */
+class MeshTest: public Game
+{
+public:
+
+    /**
+     * Constructor.
+     */
+    MeshTest();
+
+    /**
+     * @see Game::keyEvent
+     */
+	void keyEvent(Keyboard::KeyEvent evt, int key);
+	
+    /**
+     * @see Game::touchEvent
+     */
+    void touchEvent(Touch::TouchEvent evt, int x, int y, unsigned int contactIndex);
+
+protected:
+
+    /**
+     * @see Game::initialize
+     */
+    void initialize();
+
+    /**
+     * @see Game::finalize
+     */
+    void finalize();
+
+    /**
+     * @see Game::update
+     */
+    void update(float elapsedTime);
+
+    /**
+     * @see Game::render
+     */
+    void render(float elapsedTime);
+
+private:
+
+    /**
+     * Draws the scene each frame.
+     */
+    bool drawScene(Node* node);
+
+    Scene* _scene;
+};
+
+#endif
